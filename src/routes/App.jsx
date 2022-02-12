cbbb72485633a095c8c5de8b028fad346c86ba89
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-02-12 10:03:09 -0500

    lalal

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
 	const initialState = useInitialState();
 	return (
 		<AppContext.Provider value={initialState}>
-			<BrowserRouter>
+			<BrowserRouter >
 				<Layout>
 				<Routes>
 					<Route exact path="/" element={<Home />} />
--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/'
+		filename: 'copia-repo-platzi/bundle.js',
+		publicPath: '/copia-repo-platzi/'
 	},
 	mode: 'development',
 	resolve: {
